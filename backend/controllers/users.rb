e6e2221d6ec8b816746b9f0ddb0a9c24b46bd89d
Ryan Rotter <rrotter@umich.edu>
2018-02-26 21:04:40 +0000

    removed dead code in backend user controller

--- backend/controllers/users.rb
+++ backend/controllers/users.rb
@@ -28,39 +28,4 @@ class ArchivesSpaceService < Sinatra::Base
     end
   end
 
-  Endpoint.post('/remote_user/users')
-    .description("Create a remote_user user")
-    .params(["groups", [String], "Array of groups URIs to assign the user to", :optional => true],
-            ["user", JSONModel(:user), "The record to create", :body => true])
-    .permissions([])
-    .returns([200, :created],
-             [400, :error]) \
-  do
-    if env['HTTP_X_FORWARDED_SERVER']
-      # We are proxied, this is not a local connection. Kill it.
-      json_response({:error => "Denied"}, 403)
-    else
-      params[:user].username = Username.value(params[:user].username)
-
-      user = User.create_from_json(params[:user], :source => "local")
-
-      groups = Array(params[:groups]).map {|uri|
-        group_ref = JSONModel.parse_reference(uri)
-        repo_id = JSONModel.parse_reference(group_ref[:repository])[:id]
-
-        RequestContext.open(:repo_id => repo_id) do
-          if current_user.can?(:manage_repository)
-            Group.get_or_die(group_ref[:id])
-          else
-            raise AccessDeniedException.new
-          end
-        end
-      }
-
-      user.add_to_groups(groups)
-
-      created_response(user, params[:user])
-    end
-  end
-
 end
